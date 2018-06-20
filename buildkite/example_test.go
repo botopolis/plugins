359ee89cd34c22816bcfc06cf39b2126a202366f
Bernardo Farah <ber@bernardo.me>
2018-06-20 02:49:08 -0700

    Handle skipped

--- buildkite/example_test.go
+++ buildkite/example_test.go
@@ -3,6 +3,7 @@ package buildkite_test
 import (
 	"fmt"
 	"os"
+	"sync"
 
 	"github.com/botopolis/bot"
 	"github.com/botopolis/plugins/buildkite"
@@ -10,8 +11,36 @@ import (
 	oslack "github.com/nlopes/slack"
 )
 
+type skipped struct {
+	emailsPerPipeline map[string][]string
+	mu                sync.Mutex
+}
+
+func newSkipped() *skipped {
+	return &skipped{
+		emailsPerPipeline: map[string][]string{},
+	}
+}
+func (s *skipped) Add(pipeline, email string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.emailsPerPipeline[pipeline] = append(s.emailsPerPipeline[pipeline], email)
+}
+
+func (s *skipped) Pop(pipeline string) (emails []string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	emails = s.emailsPerPipeline[pipeline]
+	delete(s.emailsPerPipeline, pipeline)
+	return emails
+}
+
 func Example() {
 	chat := slack.New(os.Getenv("SLACK_TOKEN"))
+
+	skpd := newSkipped()
 
 	stateColor := func(state buildkite.State) string {
 		if state == buildkite.Failed || state == buildkite.Canceled {
@@ -27,8 +56,8 @@ func Example() {
 			return
 		}
 
-		user, ok := chat.Store.UserByEmail(be.Build.Creator.Email)
-		if !ok {
+		if be.Build.State == buildkite.Skipped {
+			skpd.Add(be.Pipeline.ID, be.Build.Creator.Email)
 			return
 		}
 
@@ -42,8 +71,8 @@ func Example() {
 		params := oslack.PostMessageParameters{
 			Attachments: []oslack.Attachment{{
 				Color:      stateColor(be.Build.State),
-				Fallback:   fmt.Sprintf("Build %s", be.Build.State),
-				Title:      fmt.Sprintf("Build %s", be.Build.State),
+				Fallback:   fmt.Sprintf("[%s] Build %s", be.Pipeline.Name, be.Build.State),
+				Title:      fmt.Sprintf("[%s] Build %s", be.Pipeline.Name, be.Build.State),
 				TitleLink:  be.Build.WebURL,
 				Text:       fmt.Sprintf("[%s]: %s", be.Build.Branch, be.Build.Commit),
 				AuthorName: "buildkite",
@@ -51,10 +80,17 @@ func Example() {
 			}},
 		}
 
-		chat.Direct(bot.Message{
-			User:   user.ID,
-			Params: params,
-		})
+		emails := append(skpd.Pop(be.Pipeline.ID), be.Build.Creator.Email)
+		for _, email := range emails {
+			user, ok := chat.Store.UserByEmail(email)
+			if !ok {
+				continue
+			}
+			chat.Direct(bot.Message{
+				User:   user.ID,
+				Params: params,
+			})
+		}
 	})
 
 	robot := bot.New(
